f1a38b3bbdc6c80c9266e9f680f9e4fc51fb252f
LuisHerasme <luis.alberto.herasme.cuevas@gmail.com>
2017-12-17 15:02:21 -0400

    Vector 2D, Vector3D
    Separation of types of vectors

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+
+const path = require('path')
+
+module.exports = {
+  context: __dirname,
+  entry: './src/index.ts',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: 'ts-loader' }
+    ]
+  }
+}
