f7daef4a1b4299a0025693b946c2e5a9e36cfac9
LuisHerasme <luis.alberto.herasme.cuevas@gmail.com>
2017-12-20 15:03:34 -0400

    Patch

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,4 +1,14 @@
-/******/ (function(modules) { // webpackBootstrap
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["Vector"] = factory();
+	else
+		root["Vector"] = factory();
+})(this, function() {
+return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
 /******/
@@ -314,3 +324,4 @@ exports["default"] = Vector3D;
 
 /***/ })
 /******/ ]);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,11 @@ module.exports = {
   entry: './src/index.ts',
   output: {
     path: path.join(__dirname, '/dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    // export itself to a global var
+    libraryTarget: 'umd',
+    // name of the global var: "Foo"
+    library: 'Vector'
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js']
